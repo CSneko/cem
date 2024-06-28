dac2684a0faed7d0a267a9783ac18c2dfa955bcb
CSneko <minecrafthetun@outlook.com>
2024-06-28 15:56:37 +0800

    init

---
+++ src/main/java/org/cneko/cem/Cem.java
@@ -0,0 +1,10 @@
+package org.cneko.cem;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Cem implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/org/cneko/cem/client/CemClient.java
@@ -0,0 +1,10 @@
+package org.cneko.cem.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CemClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
